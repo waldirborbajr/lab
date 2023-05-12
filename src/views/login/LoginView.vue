1413824af9647a8d4d65f0ef616dbe2d0c380c50
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-12 13:22:54 -0300

    feat: bank input
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bp-section">
+  <div class="bp-section">
     <h3 class="bp-form-label">Login</h3>
     <div class="bp-grid">
       <div class="bp-field-div"><label class="bp-label" for="login">Username<input type="text" class="bp-input"></label>
@@ -19,7 +19,7 @@
       <button class="bp-button bp-bt-cancel">Cancel</button>
       <button class="bp-button bp-bt-save" type="submit">Login </button>
     </div>
-  </section>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
