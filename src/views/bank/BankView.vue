f6488e8e401ca25c734c92ab07897f951ca3917e
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-13 21:03:23 -0300

    feat: grid list
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- src/views/bank/BankView.vue
+++ src/views/bank/BankView.vue
@@ -36,26 +36,32 @@
 
     <!-- Grid Cell -->
     <div class="bp-section">
-      <div class="grid grid-cols-4 gap-1">
 
-        <div class="bp-font-grid bg-blue-600">Bank</div>
-        <div class="bp-font-grid bg-blue-600">Agency</div>
-        <div class="bp-font-grid bg-blue-600">Acount</div>
-        <div class="bp-font-grid bg-blue-600">Delete</div>
+      <table class="table-auto table-striped">
+        <thead>
+          <tr>
+            <th>Bank</th>
+            <th>Agency</th>
+            <th>Account</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(bank, index) in banks" :key=index>
+            <td>{{ bank['name'] }}</td>
+            <td>{{ bank['agency'] }}</td>
+            <td>{{ bank['account'] }}</td>
+            <td>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-        <section v-for="(bank, index) in banks" :key=index>
-          <div class="bp-font-grid bg-slate-300">{{ bank['name'] }}</div>
-          <div class="bp-font-grid bg-slate-300">{{ bank['agency'] }}</div>
-          <div class="bp-font-grid bg-slate-300">{{ bank['account'] }}</div>
-          <div class="bp-font-grid bg-slate-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-              stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-            </svg>
-          </div>
-        </section>
-
-      </div>
     </div>
   </div>
 </template>
