d32761baa3d5f69f832028c69af749a4ddf389b8
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-16 17:03:42 -0300

    feat: add load message
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- src/views/bank/BankView.vue
+++ src/views/bank/BankView.vue
@@ -4,40 +4,52 @@
     <!-- Form Cell -->
     <div class="bp-section">
       <h2 class="bp-form-label">Bank Form</h2>
-      <!--      <form @submit.prevent="onSubmit"> -->
-      <div>
-        <div class="bp-field-div">
-          <label class="bp-label" for="name">Bank:</label>
-          <input class="bp-input" v-model.trim="data.name" id="name" type="text" autofocus required
-            placeholder="Bank name..." />
-          <div class="text-sm font-semibold text-red-700">Please provide bank name</div>
+      <form action="submit">
+        <div>
+          <div class="bp-field-div">
+            <label class="bp-label" for="name">Bank:</label>
+            <input class="bp-input" v-model.trim="data.name" id="name" type="text" autofocus required
+              placeholder="Bank name..." />
+            <div class="text-sm font-semibold text-red-700" v-if="v$?.name?.$dirty && v$?.name?.$error">
+              Please, Bank Name it is required
+            </div>
+          </div>
+          <div class="bp-field-div">
+            <label class="bp-label" for="agency">Agency:</label>
+            <input v-model.trim="data.agency" id="agency" class="bp-input" type="text" autofocus required
+              placeholder="Agency number..." />
+            <div class="text-sm font-semibold text-red-700" v-if="v$?.agency?.$dirty && v$?.agency?.$error">
+              Please, Bank Agency Code it is required
+            </div>
+          </div>
+          <div class="bp-field-div">
+            <label class="bp-label" for="account">Account:</label>
+            <input v-model.trim="data.account" id="account" class="bp-input" type="text" autofocus required
+              placeholder="Account number..." />
+            <div class="text-sm font-semibold text-red-700" v-if="v$?.account?.$dirty && v$?.account?.$error">
+              Please, Bank Account Number it is required
+            </div>
+          </div>
         </div>
-        <div class="bp-field-div">
-          <label class="bp-label" for="agency">Agency:</label>
-          <input v-model.trim="data.agency" id="agency" class="bp-input" type="text" autofocus required
-            placeholder="Agency number..." />
-          <div class="text-sm font-semibold text-red-700">Please provide bank agency number</div>
-        </div>
-        <div class="bp-field-div">
-          <label class="bp-label" for="account">Account:</label>
-          <input v-model.trim="data.account" id="account" class="bp-input" type="text" autofocus required
-            placeholder="Account number..." />
-          <div class="text-sm font-semibold text-red-700">Please provide bank count number</div>
-        </div>
-      </div>
 
-      <!-- BUTTONS -->
-      <div class="bp-div-three-button">
-        <!-- <BPButton class="bp-button bp-bt-new" text="Click" @buttonClicked="showAlert" /> -->
-        <button class="bp-button bp-bt-new" @click="onNewClick">New</button>
-        <button class="bp-button bp-bt-cancel" @click="onNewClick">Cancel</button>
-        <button class="bp-button bp-bt-save" @click="onSubmitClick">Save</button>
-      </div>
-      <!---      </form> -->
+        <!-- BUTTONS -->
+        <div class="bp-div-three-button">
+          <!-- <BPButton class="bp-button bp-bt-new" text="Click" @buttonClicked="showAlert" /> -->
+          <button class="bp-button bp-bt-new" @click="onNewClick">New</button>
+          <button class="bp-button bp-bt-cancel" @click="onNewClick">Cancel</button>
+          <button class="bp-button bp-bt-save" @click="onSubmitClick" type="submit">Save</button>
+        </div>
+      </form>
     </div>
 
     <!-- Grid Cell -->
     <div class="bp-section">
+      <div>
+        <button type="button" class="bg-indigo-500 bp-button" disabled v-show="data.loading">
+          <svg class="mr-3 w-5 h-5 animate-spin" viewBox="0 0 24 24"></svg>
+          Loading data...
+        </button>
+      </div>
       <table class="w-full bg-white table-auto table-striped border-collapse: collpase">
         <thead class="bg-slate-700 text-slate-300">
           <tr class="align-left items-left">
@@ -69,6 +81,8 @@
 import { onBeforeMount, reactive, onMounted } from 'vue'
 import { Trash2, Edit } from 'lucide-vue-next'
 // import BPButton from '../../components/button/BPButton.vue'
+import { required, integer, minLength } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
 import http from '../../common/http-common.ts'
 
 // Used to build grid
@@ -76,15 +90,24 @@ import http from '../../common/http-common.ts'
 onBeforeMount(async () => {
   await browseList()
 })
-// --> Grid
+// --> Grido
 
 let data = reactive({
   banks: [],
   id: null,
   name: '',
   agency: '',
-  account: ''
+  account: '',
+  loading: false
 })
+
+const rules = {
+  name: { required },
+  agency: { required, integer, minLength: minLength(2) },
+  account: { required, integer, minLength: minLength(2) }
+}
+
+const v$ = useVuelidate(rules, data)
 
 const clearFiel = () => {
   data.id = null
@@ -98,10 +121,12 @@ onMounted(() => {
 })
 
 const browseList = async () => {
+  data.loading = true
   await http
     .get('/bank')
     .then((response) => {
       data.banks = response.data.data
+      data.loading = false
     })
     .catch((error) => {
       console.log(error)
@@ -113,7 +138,6 @@ const browseList = async () => {
 // }
 
 const onNewClick = () => {
-  console.log('onNewClick')
   clearFiel()
 }
 
@@ -162,7 +186,7 @@ const editBank = async (id: BigInt) => {
     .then((response) => {
       // bankModel.value = response.data.data
       console.log(response.data)
-      data = { ...response.data.data }
+      data = response.data.data
     })
     .catch((error) => {
       console.log(error)
